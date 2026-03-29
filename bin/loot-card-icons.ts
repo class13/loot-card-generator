f1de6a48270a341bb9fecb5dff24a34126c232f9
lucky <wiiinator1@gmail.com>
2026-03-29 22:55:31 +0200

    removed unnecessary files

--- bin/loot-card-icons.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { runIconGenerator } from '../src/icon-cli.js';
-
-runIconGenerator();
--- bin/loot-card-prompts.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { runPromptGenerator } from '../src/prompt-cli.js';
-
-runPromptGenerator();
